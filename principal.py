3b6bce3ac10bef730c0092fbfb7e2a082746c685
Alumno <alumno@cepibase.int>
2022-09-17 10:11:21 +0200

    he añadido 2 nuevas funciones

--- principal.py
+++ principal.py
@@ -9,6 +9,11 @@ from werkzeug.utils import secure_filename
 import os
 from flask_mail import Mail, Message
 
+def usuario():
+	pass
+def password():
+	pass
+
 EXTENSIONES=["png","jpg","jpeg"]
 app= Flask(__name__)
 app.config["UPLOAD_FOLDER"]='./static/fondos'
